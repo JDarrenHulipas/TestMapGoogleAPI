891a379dc3b8995d9bfef8cb9655d8c0073b77a2
John Darren Hulipas <darrenhulipas@gmail.com>
2019-03-11 11:48:06 +0100

    Initial Commit

---
+++ TestMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMap
+//
+//  Created by John Darren on 11/03/2019.
+//  Copyright © 2019 Nerrad Sapiluh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
